d221684f85ea7a3ba84c58ffd8bf1e045f843b04
ace34TT <tafinasoa35@gmail.com>
2024-02-24 09:53:38 +0300

    * fix(configs): updated Twilio authToken

--- src/configs/twilio.configs.ts
+++ src/configs/twilio.configs.ts
@@ -1,5 +1,5 @@
 const accountSid = "ACfea9ff4d82ebcb53adc22265e793f8ab";
-const authToken = "4f7b2608ca3081568adb95af5d24f16d";
+const authToken = "51ce4217a2ac10791144dfd28748000a";
 
 const client = require("twilio")(accountSid, authToken);
 
