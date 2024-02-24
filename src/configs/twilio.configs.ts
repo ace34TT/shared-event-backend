b8c58eeb7bd7e5f106e1ea88952421da7f6fa6a7
ace34TT <tafinasoa35@gmail.com>
2024-02-24 13:34:52 +0300

    .env access

--- src/configs/twilio.configs.ts
+++ src/configs/twilio.configs.ts
@@ -1,5 +1,7 @@
-const accountSid = "ACfea9ff4d82ebcb53adc22265e793f8ab";
-const authToken = "51ce4217a2ac10791144dfd28748000a";
+require("dotenv").config();
+
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_AUTH;
 
 const client = require("twilio")(accountSid, authToken);
 
